4697281aa8350bac3b18c8dc0b627ff387a909be
GMargherini <giorgio.margherini@gmail.com>
2025-08-19 18:43:15 +0200

    update

--- flake.nix
+++ flake.nix
@@ -8,36 +8,32 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixgl = {
-      url = "github:nix-community/nixGL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
-    inputs @ { nixpkgs, home-manager, nixgl, ... }:
+    inputs @ { nixpkgs, home-manager, ... }:
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
       };
     in
     {
-	nixosConfigurations = {
-		mainframe = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				./configuration.nix
-				home-manager.nixosModules.home-manager {
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.dolphin = ./home/home.nix;
-					home-manager.backupFileExtension = "bak";
-					home-manager.extraSpecialArgs = {
-					    inherit inputs;
-				  	};
-				}
-			];
-		};
-	};
+      nixosConfigurations = {
+      	specialArgs = { inherit inputs; };
+	      mainframe = nixpkgs.lib.nixosSystem {
+		system = "x86_64-linux";
+		modules = [
+			./configuration.nix
+			home-manager.nixosModules.home-manager {
+			  home-manager.useGlobalPkgs = true;
+			  home-manager.useUserPackages = true;
+			  home-manager.users.dolphin = ./home/home.nix;
+			  home-manager.backupFileExtension = "bak";
+			  home-manager.extraSpecialArgs = { inherit inputs; };
+			}
+		];
+
+	      };
+      };
     };
 }
