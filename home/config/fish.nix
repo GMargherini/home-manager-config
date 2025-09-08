896154198215d2622f7a153ff3894845eac66cd1
GMargherini <giorgio.margherini@gmail.com>
2025-09-08 11:44:34 +0200

    update

--- home/config/fish.nix
+++ home/config/fish.nix
@@ -5,8 +5,6 @@
 			set fish_greeting 
 
       alias mpv_cam "mpv av://v4l2:/dev/video0"
-			starship preset nerd-font-symbols -o ~/.config/starship.toml
-			starship init fish | source
 		'';
 	};
 }
