14941f7ece5628c04fdf5b42127934aff3e50d28
GMargherini <giorgio.margherini@gmail.com>
2025-10-06 16:01:04 +0200

    set numlock

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -124,7 +124,8 @@
 
         input = {
           kb_layout = "us";
-          kb_options = compose:rctrl;
+          kb_options = "compose:rctrl";
+          numlock_by_default = true;
           follow_mouse = 1;
 
           sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
