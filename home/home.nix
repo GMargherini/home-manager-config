6f20aed9ed4cac15dda4d39d4313e41f2c04b249
GMargherini <giorgio.margherini@gmail.com>
2025-10-06 14:12:24 +0200

    removed anyrun

--- home/home.nix
+++ home/home.nix
@@ -20,7 +20,6 @@
   # environment.
   home.packages = with pkgs; [
     adwaita-icon-theme
-    anyrun
     audacity
     bat
     calc
@@ -99,28 +98,13 @@
     userEmail = "giorgio.margherini@gmail.com";
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
   # through Home Manager then you have to manually source 'hm-session-vars.sh'
   # located at either
   #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  # ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   #
   # or
   #
@@ -134,7 +118,6 @@
     # EDITOR = "emacs";
   };
     imports = [
-    (import ./anyrun/default.nix {pkgs = pkgs;})
     ./waybar/default.nix
     ./swaync/default.nix
     ./neovim/default.nix
