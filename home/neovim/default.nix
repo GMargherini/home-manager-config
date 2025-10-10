8608b79fe3bf7dc2c6ed1a6129ec44f109bdba9f
GMargherini <giorgio.margherini@gmail.com>
2025-10-10 14:13:28 +0200

    update Fri Oct 10 14:13:28 CEST 2025

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,32 +1,25 @@
 { config, lib, pkgs, ... }:
 {
   programs.neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      plugins = with pkgs.vimPlugins; [
-        nvim-lspconfig
-        nvim-treesitter.withAllGrammars
-        plenary-nvim
-        gruvbox-material
-        mini-nvim
-        surround
-      ]; 
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      nvim-treesitter.withAllGrammars
+      plenary-nvim
+      gruvbox-material
+      mini-nvim
+      surround
+      rustaceanvim
+    ]; 
   extraLuaConfig = /* lua */ ''
     require("config.lazy")
     vim.o.termguicolors  = true
     vim.g.lazyvim_rust_diagnostics = "rust-analyzer" 
     opts = {
       server = {
-        on_attach = function(_, bufnr)
-          vim.keymap.set("n", "<leader>cR", function()
-            vim.cmd.RustLsp("codeAction")
-          end, { desc = "Code Action", buffer = bufnr })
-          vim.keymap.set("n", "<leader>dr", function()
-            vim.cmd.RustLsp("debuggables")
-          end, { desc = "Rust Debuggables", buffer = bufnr })
-        end,
         default_settings = {
           -- rust-analyzer language server configuration
           ["rust-analyzer"] = {
