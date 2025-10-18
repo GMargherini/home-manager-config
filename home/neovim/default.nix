1c58e181183809776773e8a6894c3fbc8585a79c
GMargherini <giorgio.margherini@gmail.com>
2025-10-18 14:44:37 +0200

    changed neovim config

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -8,59 +8,16 @@
     plugins = with pkgs.vimPlugins; [
       nvim-lspconfig
       nvim-treesitter.withAllGrammars
-      plenary-nvim
-      gruvbox-material
       mini-nvim
       surround
       rustaceanvim
     ]; 
   extraLuaConfig = /* lua */ ''
-    require("config.lazy")
-    vim.o.termguicolors  = true
-    vim.g.lazyvim_rust_diagnostics = "rust-analyzer" 
-    opts = {
-      server = {
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = {
-                enable = true,
-              },
-            },
-            -- Add clippy lints for Rust if using rust-analyzer
-            checkOnSave = diagnostics == "rust-analyzer",
-            -- Enable diagnostics if using rust-analyzer
-            diagnostics = {
-              enable = diagnostics == "rust-analyzer",
-            },
-            procMacro = {
-              enable = true,
-              ignored = {
-                ["async-trait"] = { "async_trait" },
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-            files = {
-              excludeDirs = {
-                ".direnv",
-                ".git",
-                ".github",
-                ".gitlab",
-                "bin",
-                "node_modules",
-                "target",
-                "venv",
-                ".venv",
-              },
-            },
-          },
-        },
-      },
-    }
-  '';
+      require("config.lazy")
+      require("nvim-surround")
+      require("rustaceanvim")
+
+      vim.o.termguicolors  = true
+    '';
   };
 }
